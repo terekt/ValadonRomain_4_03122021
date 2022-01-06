3edb4ad3893d8139aba26b0a1c9ac6cf41748049
Romainv78 <romainvaladon@gmail.com>
2022-01-06 11:12:59 +0100

    Netoyage du js et ajout de commentaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,7 +24,7 @@ const regTourney = /^\d+$/;
 const regBirth = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
 const regMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-// Error messages
+// Messages d'erreur
 const error1 = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
 const error2 = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
 const error3 = "Veuillez entrer une adresse mail valide.";
@@ -36,18 +36,19 @@ const error7 = "Veuillez acceptez les termes et conditions.";
 // Les états des erreurs
 let errorState = [false, false, false, false, false, false, false]
 
-// launch modal event
+// Event de lancement de la modal 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// close modal event
+// Event de fermeture de la modal 
 modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
-// validate form event
+// Event de validation du formulaire
 submitForm.addEventListener("click", validateForm);
 
-// close modal event after validation
+// Event de fermeture de la modal après validation
 submitFinal.addEventListener("click", closeModal);
 
+// Event pour quand on quitte chaque champ
 document.getElementById("first").addEventListener("blur", nameValid);
 document.getElementById("last").addEventListener("blur", surnameValid);
 document.getElementById("email").addEventListener("blur", emailValid);
@@ -55,12 +56,12 @@ document.getElementById("birthdate").addEventListener("blur", birthValid);
 document.getElementById("quantity").addEventListener("blur", tourneyValid);
 document.getElementById("checkbox1").addEventListener("change", generalValid);
 
-// launch modal form
+// Lancer la modal de formulaire
 function launchModal() {
     modalbg.style.display = "block";
 }
 
-// close modal form
+// Fermer la modal de formulaire
 function closeModal() {
     resetData();
     modalbg.style.display = "none";
@@ -77,31 +78,25 @@ function validateForm(x) {
 // Empèche l'envoi du formulaire si les conditions ne sont pas bonnes
 function checkFields() {
 
-    //On récupère la présence d'une donnée pour chaque champs dans une array
+    // On récupère la présence d'une donnée pour chaque champs dans une array
     let inputList = [];
     inputList.push(formInput[0].value.length, formInput[1].value.length, formInput[2].value.length, formInput[3].value.length, formInput[4].value.length, formInput[1]);
 
-    //On récupère les resultats de chaque champ dans une array
+    // On récupère les resultats de chaque champ dans une array
     let validList = [nameValid(), surnameValid(), emailValid(), birthValid(), tourneyValid(), townValid(townRadio), generalValid()];
 
-    //on vérifie si les champs sont vides et erronés
+    // On vérifie si les champs sont vides et erronés
     if (inputList.includes(0) || validList.includes(false)) {
-        console.log("champ vide");
-        console.log(validList);
-        console.log(errorState);
         return false;
     }
     else {
-        console.log("champ ok !");
-        console.log(validList);
-        console.log(errorState);
         modalBody.style.display = "none";
         modalValid.style.display = "flex";
         return true;
     }
 }
 
-//reset les données du formulaire
+// Reset les données du formulaire
 function resetData() {
     document.getElementById("myForm").reset();
     errorReset(formData[0], 0);
@@ -114,7 +109,7 @@ function resetData() {
 }
 
 
-//validation Prénom
+// Validation Prénom
 function nameValid() {
     if (formInput[0].value.length > 1) { // si le prénom fait plus de 1 charactère
         errorReset(formData[0], 0); // reset l'erreur 1
@@ -126,7 +121,7 @@ function nameValid() {
     }
 }
 
-//validation Nom de famille
+// Validation Nom de famille
 function surnameValid() {
     if (formInput[1].value.length > 1) {
         errorReset(formData[1], 1);
@@ -138,7 +133,7 @@ function surnameValid() {
     }
 }
 
-//validation email
+// Validation email
 function emailValid() {
     if (regMail.test(formInput[2].value)) {
         errorReset(formData[2], 2);
@@ -150,7 +145,7 @@ function emailValid() {
     }
 }
 
-//validation Date de naissance
+// Validation Date de naissance
 function birthValid() {
     if (regBirth.test(formInput[3].value)) {
         errorReset(formData[3], 3);
@@ -162,7 +157,7 @@ function birthValid() {
     }
 }
 
-//validation concours
+// Validation concours
 function tourneyValid() {
     if (regTourney.test(formInput[4].value)) {
         errorReset(formData[4], 4);
@@ -174,7 +169,7 @@ function tourneyValid() {
     }
 }
 
-//validation ville
+// Validation ville
 function townValid(i) {
     if (document.querySelectorAll('[name="location"]:checked').length > 0) {
         errorReset(formData[5], 5);
@@ -186,16 +181,14 @@ function townValid(i) {
     }
 }
 
-//validation conditions générales
+// Validation conditions générales
 function generalValid() {
     if (document.getElementById('checkbox1').checked) {
         errorReset(formData[6], 6);
-        console.log("true");
         return true;
     }
     else {
         errorMessage(error7, formData[6], 6);
-        console.log("false");
         return false
     }
 } 
@@ -203,16 +196,16 @@ function generalValid() {
 // Génération du message d'erreur
 function errorMessage(errorText, formNumber, errorNumber) {
 
-    var newDiv = document.createElement("div"); // créé une variable contenant une création d'une div
-    var newContent = document.createTextNode(errorText); // créé une variable contenant une du texte reprenant la valeur spécifié
+    var newDiv = document.createElement("div"); // Créé une variable contenant une création d'une div
+    var newContent = document.createTextNode(errorText); // Créé une variable contenant une du texte reprenant la valeur spécifié
 
-    if (errorState[errorNumber] == false){ // si l'erreur avec le numéro spécifié n'est pas affiché
-    newDiv.appendChild(newContent); // assigne le texte à la div
-    newDiv.setAttribute('class', 'data-error') // donne la classe data-error à la div
+    if (errorState[errorNumber] == false){ // Si l'erreur avec le numéro spécifié n'est pas affiché
+    newDiv.appendChild(newContent); // Assigne le texte à la div
+    newDiv.setAttribute('class', 'data-error') // Donne la classe data-error à la div
 
-    formNumber.parentNode.insertBefore(newDiv, formNumber.nextSibling); // insère la div après l'input spécifié
+    formNumber.parentNode.insertBefore(newDiv, formNumber.nextSibling); // Insère la div après l'input spécifié
 
-    errorState[errorNumber] = true; // l'erreur est définie comme affichée
+    errorState[errorNumber] = true; // L'erreur est définie comme affichée
     }
     else {
     }
@@ -222,9 +215,9 @@ function errorMessage(errorText, formNumber, errorNumber) {
 // Détruit l'erreur affichée
 function errorReset(formNumber, errorNumber) {
 
-    if (errorState[errorNumber] == true) { // si l'erreur est définie comme affichée
-        formNumber.parentNode.removeChild(formNumber.nextSibling); // enlève la div après l'input spécifié
-        errorState[errorNumber] = false; // l'erreur est définie comme cachée
+    if (errorState[errorNumber] == true) { // Si l'erreur est définie comme affichée
+        formNumber.parentNode.removeChild(formNumber.nextSibling); // Enlève la div après l'input spécifié
+        errorState[errorNumber] = false; // L'erreur est définie comme cachée
     }
     else {
     }
